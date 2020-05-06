ac5506a60735ca0c575e56b9abfae15ad0036428
ChrisBailey-ZA <kommetjiechris@yahoo.com>
2020-05-06 22:06:27 +0200

    bit of a cleanup

--- themes/aero/assets/js/script.js
+++ themes/aero/assets/js/script.js
@@ -1,42 +1,7 @@
 // Preloader js    
-$(window).on('load', function () {
-	$('.preloader').fadeOut(100);
-});
 
 $(document).ready(function () {
 	'use strict';
-
-	// Shuffle js filter and masonry
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
-
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
-
-	$('.portfolio-single-slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-
-	});
-
-	$('.clients-logo').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
 
 	$('.testimonial-slider').slick({
 		slidesToShow: 1,
@@ -55,24 +20,6 @@ $(document).ready(function () {
 		adaptiveHeight: true,
 		centerMode: true,
 		centerPadding: '60px',
-	});
-
-
-	// Magnific Popup Image
-	$('.portfolio-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: true,
-		fixedBgPos: true
 	});
 
 	//  Count Up
@@ -123,6 +70,7 @@ $(document).ready(function () {
 			"Accept": "application/json"
 		  }
 		}).done(function() {
+			dataLayer.push({'event': 'formSubmit'})
 		   $('.form-success').addClass('is-active');
 		   $('#contactForm').addClass('submitted');
 		}).fail(function() {
